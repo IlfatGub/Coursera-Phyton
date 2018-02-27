56063a7502b9575809f98f1c77a79cf88e54448d
Ilfat <ilfat.gubaidullin@gmail.com>
2018-02-27 14:44:03 +0500

    ----

--- week3/Квадратное уравнение - 1.py
+++ week3/Квадратное уравнение - 1.py
@@ -3,13 +3,17 @@ import math
 a = float(input())
 b = float(input())
 c = float(input())
-di = b**2 - 4 * a * c
+di = (b**2) - (4 * a * c)
 if di > 0:
-    x1 = (-b + math.sqrt(di)) / (2 * a)
-    x2 = (-b - math.sqrt(di)) / (2 * a)
+    x1 = ((-1) * b + math.sqrt(di)) / (2 * a)
+    x2 = ((-1) * b - math.sqrt(di)) / (2 * a)
     if x1 > x2:
         (x1, x2) = (x2, x1)
-        print(x1, x2)
+        print(round(x1, 6), round(x2, 6))
 elif di == 0:
-    x = -b / (2 * a)
+    x = -b / 2 / a
     print(x)
+else:
+    print()
+
+
