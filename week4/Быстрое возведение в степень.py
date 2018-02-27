21b0db6e3e3dc3b0d744adf41c7c823bbbdec17a
Ilfat <ilfat.gubaidullin@gmail.com>
2018-02-27 15:00:20 +0500

    ++++

--- week4/Быстрое возведение в степень.py
+++ week4/Быстрое возведение в степень.py
@@ -1,14 +1,19 @@
 # Быстрое возведение в степень
-a = float(input())
-n = int(input())
+x = float(input())
+p = int(input())
 
 
-def power(a, n):
-    if n == 0:
-        return 1
-    elif n % 2 == 1:
-        return a * power(a, n - 1)
-    else:
-        b = power(a, n/2)
-        return b * b
-print(power(a, n))
+def pow(x, p):
+    if p < 0:
+        p = -p
+        x = 1. / x
+    m = x
+    t = 1
+    while p:
+        if p % 2:
+            t *= m
+        m *= m
+        p //= 2
+    return t
+
+print(pow(x, p))
