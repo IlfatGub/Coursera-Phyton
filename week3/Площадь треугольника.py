eb475772cb8fd85514c61363235b539fead2bcf9
Ilfat <ilfat.gubaidullin@gmail.com>
2018-02-27 14:44:27 +0500

    ++++

--- week3/Площадь треугольника.py
+++ week3/Площадь треугольника.py
@@ -1,8 +1,8 @@
 # Площадь треугольника
 import math
-a = int(input())
-b = int(input())
-c = int(input())
+a = float(input())
+b = float(input())
+c = float(input())
 p = (a + b + c) / 2.0
 s = math.sqrt(p * (p - a) * (p - b) * (p - c))
 print(s)
