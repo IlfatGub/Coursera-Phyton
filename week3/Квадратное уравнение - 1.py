884398e7f22c0a28e5bd9db4a69d3ff1cd22e657
Ilfat <ilfat.gubaidullin@gmail.com>
2018-02-27 15:12:32 +0500

    ++++

--- week3/Квадратное уравнение - 1.py
+++ week3/Квадратное уравнение - 1.py
@@ -3,17 +3,28 @@ import math
 a = float(input())
 b = float(input())
 c = float(input())
-di = (b**2) - (4 * a * c)
-if di > 0:
-    x1 = ((-1) * b + math.sqrt(di)) / (2 * a)
-    x2 = ((-1) * b - math.sqrt(di)) / (2 * a)
-    if x1 > x2:
-        (x1, x2) = (x2, x1)
-        print(round(x1, 6), round(x2, 6))
-elif di == 0:
-    x = -b / 2 / a
-    print(x)
+d = (b ** 2) - (4 * a * c)
+if d == 0:
+    x = -b / (2 * a)
+    if x % 1 == 0:
+        x1an = int(x)
+    else:
+        x1an = x
+    print(x1an)
+elif d < 0:
+    y = math.sqrt(0 - d)
 else:
-    print()
-
-
+    x1 = (-b + math.sqrt(d)) / (2 * a)
+    x2 = (-b - math.sqrt(d)) / (2 * a)
+    if x1 % 1 == 0:
+        x1an = int(x1)
+    else:
+        x1an = x1
+    if x2 % 1 == 0:
+        x2an = int(x2)
+    else:
+        x2an = x2
+    if x1an <= x2an:
+        print('{} {}'.format(round(x1an, 6), round(x2an, 6)))
+    else:
+        print('{} {}'.format(round(x2an, 6), round(x1an, 6)))
